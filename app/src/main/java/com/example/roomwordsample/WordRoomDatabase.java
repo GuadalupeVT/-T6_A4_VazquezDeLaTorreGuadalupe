7663726e5b2ba96bdc2bba2bb5c4138af14519ac
Guadalupe Vazquez <guvadlt@gmail.com>
2019-12-05 19:51:47 -0600

    Se agrego la clase WordRepository

--- app/src/main/java/com/example/roomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/example/roomwordsample/WordRoomDatabase.java
@@ -1,11 +1,10 @@
+package com.example.roomwordsample;
+
 import android.content.Context;
 
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-
-import com.example.roomwordsample.Word;
-import com.example.roomwordsample.WordDao;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -34,4 +33,4 @@ import java.util.concurrent.Executors;
             return INSTANCE;
         }
     }
-    
+
